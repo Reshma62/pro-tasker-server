99c517ca722a6d58ca8034b322c5c53b11b677f6
Reshma62 <mernstack2024@gmail.com>
2024-04-02 12:33:24 +0600

    added

--- utils/tokenGenarate.js
+++ utils/tokenGenarate.js
@@ -4,7 +4,7 @@ const TokenGenerate = (user, res) => {
   const data = { email: user.email, id: user._id };
   const oneDay = 24 * 60 * 60 * 1000; // One day in milliseconds
   const currentTime = Date.now(); // Current time in milliseconds
-  const expirationTime = currentTime + oneDay; // Expiration time is current time plus one day
+  // Expiration time is current time plus one day
 
   const token = jwt.sign(data, process.env.JWT_SECRET_KEY, {
     expiresIn: "1d",
@@ -12,7 +12,7 @@ const TokenGenerate = (user, res) => {
 
   // Set the cookie with a maximum age of one day and SameSite attribute
   res.cookie("token", token, {
-    maxAge: Date.now() - oneDay, // maxAge should be in milliseconds relative to
+    maxAge: oneDay, // maxAge should be in milliseconds relative to
     httpOnly: true,
     secure: true,
     sameSite: "none",
